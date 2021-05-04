738f4e5b31ba0f693cfa4f07981419e94663a7e8
Hieu Le <hieulev319@gmail.com>
2021-05-04 22:47:57 +0700

    the first commit

---
+++ app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserAuthController extends Controller
+{
+    function login()
+    {
+        return view('auth.login');
+    }
+
+    function register()
+    {
+        return view('auth.register');
+    }
+
+    function create(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|max:12'
+        ]);
+
+        $params = [
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ];
+        $query = User::insertOrUpdateUser($params);
+
+        if ($query) {
+            return back()->with('success', 'You have been successfuly registered');
+        } else {
+            return back()->with('fail', 'Something went wrong');
+        }
+    }
+
+    function check(Request $request)
+    {
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required|min:6|max:12'
+        ]);
+
+        $user = User::checkLogin($request);
+
+        if ($user) {
+            if (Hash::check($request->password, $user['password'])) {
+                $request->session()->put('LoggedUser', $user['id']);
+                return redirect('home');
+            } else {
+                return back()->with('fail', 'Invalid password');
+            }
+        } else {
+            return back()->with('fail', 'No account found for this user name');
+        }
+    }
+
+    function home()
+    {
+        if (session()->has('LoggedUser')) {
+            $data['id'] = session('LoggedUser');
+            $user = User::checkLogin($data);
+            $data = [
+                'LoggedUserInfo' => $user
+            ];
+        }
+        if ($user['permission'] == 0 || $user['permission'] == 1) {
+            return view('admin.home', $data);
+        }
+
+        if ($user['permission'] == 2) {
+            return view('user.profile', $data);
+        }
+    }
+
+    function logout()
+    {
+        if(session()->has('LoggedUser'))
+        {
+            session()->pull('LoggedUser');
+            return redirect('login');
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//User
+Route::get('login',[UserAuthController::class, 'login'])->middleware('AlreadyLoggedIn');
+Route::get('register',[UserAuthController::class, 'register'])->middleware('AlreadyLoggedIn');
+Route::post('create',[UserAuthController::class, 'create'])->name('auth.create');
+Route::post('check',[UserAuthController::class, 'check'])->name('auth.check');
+
+//Check login
+Route::middleware(['isLogged'])->group(function () {
+    Route::get('home',[UserAuthController::class, 'home']);
+    Route::get('logout',[UserAuthController::class, 'logout']);
+});
